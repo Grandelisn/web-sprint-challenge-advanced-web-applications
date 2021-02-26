1de0877dda329cf7fec64c188226b9c12c509560
nathaniel-grandelis <grandelisn@gmail.com>
2021-02-26 13:25:08 -0500

    mvp y'all

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -1,12 +1,13 @@
 import axios from "axios";
-export const axiosWithAuth = () =>{
+const axiosWithAuth = () =>{
     const token = localStorage.getItem('token');
     return axios.create({
         baseURL: "http://localhost:5000/api",
-        headers: {
-          Authorization: token,
-        },
+    headers: {
+      Authorization: token,
+    },
     })
 }
+export default axiosWithAuth;
 //Task List:
 //Build and export a function used to send in our authorization token
